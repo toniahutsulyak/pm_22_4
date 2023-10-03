b65563ee07fc9db9c7d86d02d40977cedd5d9ac1
Антоніна Гуцуляк <toniahutsulyak@gmail.com>
2023-10-03 09:59:20 +0300

    2 commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require ("gulp");
 //додаткові плагіни Gulp
-var sass = require ("gulp-sass"), //конвертує SASS в CSS
+/*var sass = require ("gulp-sass"), //конвертує SASS в CSS
     cssnano = require ("gulp-cssnano"), //мінімізація CSS
     autoprefixer = require ('gulp-autoprefixer'), //додавання префіксів в
     //CSS для підтримки
@@ -8,52 +8,82 @@ var sass = require ("gulp-sass"), //конвертує SASS в CSS
     imagemin = require ('gulp-imagemin'), //стиснення зображень
     concat = require ("gulp-concat"), //об'єднання файлів - конкатенація
     uglify = require ("gulp-uglify"), //мінімізація javascript
-    rename = require ("gulp-rename"); //перейменування файлів
+    rename = require ("gulp-rename"); //перейменування файлів*/
 
 
-gulp.task ( "html", function () {
-    return gulp.src ( "src / *. html")
-        .pipe (gulp.dest ( "dist"));
-});
+function html() {
+    return gulp.src('app/*.html')
+        .pipe(gulp.dest('dist'))
+}
+
+function sass() {
+    return gulp.src('app/*.html')
+        .pipe(gulp.dest('dist/css'))
+}
+
+
+exports.default = gulp.series(html);
+/*function html_task(cb)
+{
+    pump([
+        gulp.src ("app/!*.html"),
+        gulp.dest ("dist")
+        ],
+    cb)
+}
+exports.html = html_task;
 //об'єднання, компіляція Sass в CSS, додавання префіксів і подальша
 
-gulp.task ( "sass", function () {
-    return gulp.src ( "src / sass / *. sass")
-        .pipe (concat ( 'styles.sass'))
-        .pipe (sass ())
-        .pipe (autoprefixer ({
-            browsers: [ 'last 2 versions'],
+
+function sass_task(cb)
+{ pump([
+     gulp.src ("app/sass/!*.sass"),
+         concat ('styles.sass'),
+         sass (),
+        autoprefixer ({
+            browsers: ['last2versions'],
             cascade: false
-        }))
-        .pipe (cssnano ())
-        .pipe (rename ({suffix: '.min'}))
-        .pipe (gulp.dest ( "dist / css"));
-});
+        }),
+        cssnano (),
+        rename ({suffix:'.min'}),
+        gulp.dest ("dist/css")
+    ],
+    cb)
+}
 //об'єднання і стиснення JS-файлів
-gulp.task ( "scripts", function () {
-    return gulp.src ( "src / js / *. js") //вихідна директорія файлів
-        .pipe (concat ( 'scripts.js')) // конкатенація js-файлів в один
-        .pipe (uglify ()) //стиснення коду
-        .pipe (rename ({suffix: '.min'})) //перейменування файлу з
+
+function scripts_task(cb)
+    { pump([
+     gulp.src ("app/js/!*.js"), //вихідна директорія файлів
+        concat ('scripts.js'), // конкатенація js-файлів в один
+        uglify (), //стиснення коду
+        rename ({suffix:'.min'}), //перейменування файлу з
         //приставкою .min
-        .pipe (gulp.dest ( "dist / js")); // директорія продакшена
-});
+        gulp.dest ("dist/js")
+        ],
+        cb)
+    }// директорія продакшена
+
 //cтискання зображень
-gulp.task ( 'imgs', function () {
-    return gulp.src ( "src / images /*.+ (jpg | jpeg | png | gif)")
-        .pipe (imagemin ({
+function img_task(cb)
+
+{ pump([
+     gulp.scr ( "app/img/!*.+(jpg|jpeg|png|gif)"),
+        (imagemin ({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
             interlaced: true
         }))
-        .pipe (gulp.dest ( "dist / images"))
-});
+        (gulp.dest ( "dist/img")),
+],
+    cb);
 //відстежування за змінами у файлах
-gulp.task ( "watch", function () {
-    gulp.watch ( "src / *. html", [ "html"]);
-    gulp.watch ( "src / js / *. js", [ "scripts"]);
-    gulp.watch ( "src / sass / *. sass", [ "sass"]);
-    gulp.watch ( "src / images /*.+ (jpg | jpeg | png | gif)", [ "imgs"]);
-});
+gulp.task ("watch",function () {
+    gulp.watch ( "app/*.html",["html"]);
+    gulp.watch ( "app/js/*.js", ["scripts"]);
+    gulp.watch ( "app/sass/!*.sass",["sass"]);
+    gulp.watch ( "app/img/!*.+(jpg|jpeg|png|gif)", ["img"]);
+});*/
 //Запуск тасків за замовчуванням
-gulp.task ( "default", [ "html", "sass", "scripts", "imgs", "watch"]);
+/*
+gulp.task ("default",["html","sass","scripts","img","watch"]);}*/
